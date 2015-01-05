09589a18947fc8ec2eb3fe9f64a4031b4ba67cf3
Olle Kvarnström <olle+git@iix.se>
2015-01-05 16:13:43 +0100

    give prompt.h info

--- src/prompt.h
+++ src/prompt.h
@@ -1,6 +1,13 @@
 #ifndef PROMPT_PROMPT_H
 #define PROMPT_PROMPT_H
 
+/**
+ * prompt - read a line from the user
+ * @prompt	string to write as a prompt. NULL means no prompt.
+ *
+ * The line returned is allocated with malloc, so it must be freed when
+ * no longer needed. The newline at the end of the string is removed.
+ */
 char
 *prompt(const char *prompt);
 
